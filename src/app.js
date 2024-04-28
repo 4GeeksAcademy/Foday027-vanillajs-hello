fb94e202329988e663e5418bc9c0d1710a7b5b35
Foday027 <159056257+Foday027@users.noreply.github.com>
2024-04-28 14:12:26 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let endings = [".com", ".net", ".us", ".io"];
+
+  let alertElement = document.querySelector(".alert");
+
+  if (alertElement) {
+    alertElement.innerHTML = "";
+    for (let i = 0; i < pronouns.length; i++) {
+      let currentPronoun = pronouns[i];
+      for (let j = 0; j < adjs.length; j++) {
+        let currentAdj = adjs[j];
+        for (let k = 0; k < nouns.length; k++) {
+          let currentNoun = nouns[k];
+          for (let l = 0; l < endings.length; l++) {
+            let currentEnding = endings[l];
+            let newDomain = `${currentPronoun}${currentAdj}${currentNoun}${currentEnding}`;
+            alertElement.innerHTML += newDomain + "<br>";
+          }
+        }
+      }
+    }
+  }
 };
